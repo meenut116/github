01915dfad5b4a812691933ec14a0c75e07d7f4d6
Meenu <meenu@meenusmac.local>
2019-09-28 14:19:48 -0700

    project added

---
+++ src/test/java/Test/github/tesing.java
@@ -0,0 +1,16 @@
+package Test.github;
+
+import org.testng.annotations.Test;
+
+public class tesing {
+	
+	@Test
+	public void geturl() {
+		System.out.println("get url");
+	}
+	@Test
+	public void gettitle() {
+		System.out.println("get title");
+	}
+
+}
