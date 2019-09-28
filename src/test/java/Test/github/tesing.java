dc9408986e427c553779cd4eb6508b3b8177f3e0
Meenu <meenu@meenusmac.local>
2019-09-28 14:48:37 -0700

    add ne tc

--- src/test/java/Test/github/tesing.java
+++ src/test/java/Test/github/tesing.java
@@ -3,14 +3,20 @@ package Test.github;
 import org.testng.annotations.Test;
 
 public class tesing {
-	
+
 	@Test
 	public void geturl() {
 		System.out.println("get url");
 	}
+
 	@Test
 	public void gettitle() {
 		System.out.println("get title");
+	}
+
+	@Test
+	public void logout() {
+		System.out.println("logout");
 	}
 
 }
